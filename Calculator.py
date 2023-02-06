5e257c40e6c4d7c1a51046d6767528befbada006
Tanuj Gupta <64638288+Tanujgupta1872003@users.noreply.github.com>
2023-02-06 22:49:15 +0530

    Basic Arithematic Calculator

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+print("Select Operator")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Modulo")
+
+n1 = float(input("Enter First Number: "))
+n2 = float(input("Enter Second Number: "))
+
+choice = int(input("Enter Your Choice: "))
+if choice == 1:
+    sum = n1 + n2
+    print("Answer = ",sum)
+elif choice == 2:
+    sum = n1-n2
+    print("Answer = ",sum)
+elif choice == 3:
+    sum = n1*n2
+    print("Answer = ",sum)
+elif choice == 4:
+    sum = n1/n2
+    print("Answer = ",sum)
+elif choice == 5:
+    sum = n1%n2
+    print("Answer = ",sum)
+else:
+    print("Invalid Input")
